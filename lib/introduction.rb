8a0792b8be60d695478cb9023ed599b01f5c1a34
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-11-11 16:54:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,7 +5,7 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end 
-def introduction_with_language (name=Ninibeth,language=Ruby)
+def introduction_with_language (Ninibeth,Ruby)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
 def introduction_with_language_optional (language=Ruby)
